24f0bbc41ec1d71e7be5e9a3a0852313b74fc7a1
sihina3436 <nimnadasihina@gmail.com>
2026-03-14 01:18:06 +0530

    feat(router): modify frontend routing setup in Router.tsx

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,38 +1,57 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/Home";
-import SignIn from "../pages/SignIn";
-import SignUp from "../pages/SignUp";
-import ForgotPassword from "../pages/ForgotPassword";
-import VerifyOTP from "../pages/VerifyOTP";
-import Feed from "../pages/Feed";
+import SignIn from "../pages/auth/SignIn";
+import SignUp from "../pages/auth/SignUp";
+import ForgotPassword from "../pages/auth/ForgotPassword";
+import VerifyOTP from "../pages/auth/VerifyOTP";
+import Feed from "../pages/public/Feed";
+import Home from "../pages/public/Home";
+import AddPost from "../pages/user/post/AddPost";
+import PostDetails from "../pages/public/PostDetails";
+import ChatRequest from "../pages/user/chat/ChatRequest";
+import Chat from "../pages/user/chat/Chat";
+import UpdateProfile from "../pages/user/profile/UpdateProfile";
+import Profile from "../pages/user/profile/Profile";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {path:"/home", element:<Home /> },
-            {path:"/feed", element:<Feed /> }
-        ]
-    },
-    {
-        path:"/signin",
-        element: <SignIn />
-    },
-    {
-        path:"/signup",
-        element: <SignUp />
-    },
-    {
-        path:"/forgot-password",
-        element: <ForgotPassword />
-    },
-    {
-        path:"/verify-otp",
-        element: <VerifyOTP />
-    },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
 
+      { path: "/feed", element: <Feed /> },
 
+      { path: "/profile", element: <Profile /> },
 
+      { path: "/add-post", element: <AddPost /> },
+
+      { path: "/post/:id", element: <PostDetails /> },
+
+      {path: "/chat-requests", element: <ChatRequest/>},
+
+      {path: "/chat/:id", element: <Chat/>},
+      {path: "/update-profile", element: <UpdateProfile/>}
+    ]
+  },
+
+  {
+    path: "/signin",
+    element: <SignIn />
+  },
+
+  {
+    path: "/signup",
+    element: <SignUp />
+  },
+
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword />
+  },
+
+  {
+    path: "/verify-otp",
+    element: <VerifyOTP />
+  }
 ]);
